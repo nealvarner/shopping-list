b146251da855c64ba0a57518d2069a997a490c1b
Neal Varner <neal.varner@gmail.com>
2015-03-08 19:29:08 -0500

    Can add, strikethrough, and remove items. Can't remove items that have been added though...?

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 jQuery(document).ready(function($) {
-  console.log("STARTED")
-  $('.delete-button').on('click', 'li', function(event){
+  console.log("STARTED");
+  $('.delete-button').on('click', function(event){
   // fires when any LIs are clicked on
   // including LIs that aren't on the page when it is initially loaded
-    delete_item();
+    console.log($(this).parent('li'));
+    $(this).parent('li').remove();
   });
 
+  $('.add-button').on('click', function(event){
+    console.log("clicked add-button");
+    console.log($('.item-to-add').val());
+    $('.shop-list').append('<li class="item"><input type="checkbox" id="ch_box" value="'+$('.item-to-add').val()+'"><label>'+$('.item-to-add').val()+'</label><button class="delete-button">Remove</button></li>');
+    $('.item-to-add').val('');
+  });
 });
 
-function delete_item() {
-  console.log("delete func called")
-  $(this).parent('li').remove()
-}
 
 function add(item_name) {
-  $('#shop-list').append('<li class="item"> <input type="checkbox" name="item" value="'+item_name+'">'+item_name+'<button class="delete-button">Remove</button></li>')
+  $('#shop-list').append('<li class="item"> <input type="checkbox" name="item" value="'+item_name+'"><label>'+item_name+'</label><button class="delete-button">Remove</button></li>');
 }
